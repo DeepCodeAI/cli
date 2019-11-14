6e8c105fe6c6cb1cca7e36171f9e4b3f0389dc8a
Arvid Paeglit <arvid@arpaso.com>
2019-11-14 12:59:33 +0100

    ignored false positive

--- deepcode/tests/mocked_for_tests/sample_repository/main.js
+++ deepcode/tests/mocked_for_tests/sample_repository/main.js
@@ -14,6 +14,7 @@ var agt = navigator.userAgent.toLowerCase();
 var browserType;
 var browserVer;
 
+// file deepcode ignore ~~~~equality: wrong suggestion for Python
 if (agt.indexOf("opera") != -1) {
   browserType = "Opera";
 
