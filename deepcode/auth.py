40ce4c1d49f866186adae71346e3c6231cb1184b
Arvid Paeglit <arvid@deepcode.ai>
2020-05-04 15:16:11 +0200

    restored default value for auth.login method, as there might be existing users already

--- deepcode/auth.py
+++ deepcode/auth.py
@@ -8,10 +8,10 @@ import aiohttp
 
 from .utils import logger
 from .connection import api_call
-from .constants import AUTH_POLLING_INTERVAL
+from .constants import AUTH_POLLING_INTERVAL, DEFAULT_SOURCE
 
 
-async def login(service_url, source):
+async def login(service_url, source=DEFAULT_SOURCE):
     """
     Initiate a new login protocol.
     User will be forwarded to Deepcode website to complete the process.
@@ -25,8 +25,8 @@ async def login(service_url, source):
     while(True):
         try:
             await asyncio.sleep(AUTH_POLLING_INTERVAL)
-            status = await api_call('session', method="GET", 
-                callback=asyncio.coroutine(lambda r: r.status), 
+            status = await api_call('session', method="GET",
+                callback=asyncio.coroutine(lambda r: r.status),
                 api_key=api_key)
             if status == 304:
                 print('Please, complete login process in opened browser. Re-checking session in {} sec'.format(AUTH_POLLING_INTERVAL))
